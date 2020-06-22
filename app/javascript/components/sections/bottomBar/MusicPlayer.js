0495150ac228445fc2533dc762ef4d6c02b5385d
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-06-22 07:48:35 +0100

    code reorganization

--- app/javascript/components/sections/bottomBar/MusicPlayer.js
+++ app/javascript/components/sections/bottomBar/MusicPlayer.js
@@ -1,14 +1,9 @@
-import React, { useEffect, useState } from "react";
-import Spacing, { SPACING } from "../../layout/Spacing";
+import React from "react";
+import { SPACING } from "../../layout/Spacing";
 import { useSelector, useDispatch } from "react-redux";
 import Draggable from "../../layout/Draggable";
-import Container from "../../layout/Container";
-import Text from "../../layout/Text";
-import SVG from "../../svg/SVG";
-import Title from "../../layout/pane/Title";
-import Line from "../../layout/Line";
-import DockButton from "./musicPlayer/DockButton";
 import * as musicActions from "../../store/actions/music";
+import Player from "./musicPlayer/Player";
 
 const initialPlayerPosition = {
   absoluteLeft: `-${4 * SPACING}px`,
@@ -19,13 +14,10 @@ const MusicPlayer = () => {
   const dispatch = useDispatch();
 
   const dock = useSelector(state => state.music.dock);
-  const furthest = useSelector(state => state.theme.theme.color.furthest);
+  // const furthest = useSelector(state => state.theme.theme.color.furthest);
   const closest = useSelector(state => state.theme.theme.color.closest);
 
   const onDrag = ({ newTop, newLeft }) => {
-    console.log("dragged");
-    console.log(`newTop: ${newTop}, newLeft: ${newLeft}`);
-
     const { absoluteTop, absoluteLeft } = initialPlayerPosition;
 
     if (newTop === absoluteTop && newLeft === absoluteLeft) dispatch(musicActions.setDock(false));
@@ -43,49 +35,10 @@ const MusicPlayer = () => {
         background: closest,
         z: 2,
         onDrag,
+        children: <Player />,
       }}
-    >
-      <Player />
-    </Draggable>
+    />
   );
 };
 
 export default MusicPlayer;
-
-const Player = () => {
-  const closest = useSelector(state => state.theme.theme.color.closest);
-
-  const artist = useSelector(state => state.music.currentSong.artist);
-  const title = useSelector(state => state.music.currentSong.title);
-  // const filename = useSelector(state => state.music.currentSong.filename)
-
-  const playTime = 50; // TESTING
-  const songLength = 500; // TESTING
-
-  return (
-    <Spacing horizontal>
-      <DockButton />
-      <Line vertical />
-      <Spacing {...{ left: 1, right: 1 }}>
-        <Title
-          {...{ title: artist, subtitles: [title, `${playTime}s`, `-${songLength - playTime}s`] }}
-        />
-      </Spacing>
-      <Line vertical />
-      <Spacing
-        pointer
-        {...{ hover: <Hover />, children: <SVG {...{ name: "Note", size: 6 }} /> }}
-      />
-    </Spacing>
-  );
-};
-
-const Hover = () => {
-  return (
-    <Container border>
-      <Text extraSmall>
-        Click and drag the <Text primary extraSmall bold children="music player" />.
-      </Text>
-    </Container>
-  );
-};
