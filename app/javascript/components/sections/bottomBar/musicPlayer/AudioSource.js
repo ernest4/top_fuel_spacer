63099c2e7f886dc1ec24b214c925d0be5ad53e41
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-06-22 23:00:01 +0100

    found source of timeline control on thee audio player

--- app/javascript/components/sections/bottomBar/musicPlayer/AudioSource.js
+++ app/javascript/components/sections/bottomBar/musicPlayer/AudioSource.js
@@ -4,7 +4,10 @@ import { useSelector } from "react-redux";
 // instigate our audio context
 // for cross browser
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+const audioContext = new AudioContext();
+const gainNode = audioContext.createGain();
+var frameCount = audioContext.sampleRate * 2.0;
+const myArrayBuffer = audioContext.createBuffer(2, frameCount, audioContext.sampleRate);
 
 const AudioSource = () => {
   const audioRef = useRef(null);
@@ -12,24 +15,45 @@ const AudioSource = () => {
 
   const src = useSelector(state => state.music.currentSong.src);
   const playing = useSelector(state => state.music.playing);
-
-  // const track = audioCtx.createMediaElementSource(audioElement);
+  const volume = useSelector(state => state.music.volume);
 
   useEffect(() => {
-    if (audioRef && audioRef.current) setTrack(audioCtx.createMediaElementSource(audioRef.current));
+    if (audioRef && audioRef.current) {
+      setTrack(audioContext.createMediaElementSource(audioRef.current));
+
+      // TODO: not working...?!?!
+      // const endedCallback = () => {
+      //   console.log("song finished");
+      // };
+
+      // audioRef.current.addEventListener("ended", endedCallback, false);
+
+      // return audioRef.current.removeEventListener("ended", endedCallback);
+    }
   }, [audioRef]);
 
   useEffect(() => {
     // check if context is in suspended state (autoplay policy)
-    if (audioCtx.state === "suspended") audioCtx.resume();
+    if (audioContext.state === "suspended") audioContext.resume();
 
     if (playing) audioRef.current.play();
     else audioRef.current.pause();
   }, [playing]);
 
   useEffect(() => {
-    if (track) track.connect(audioCtx.destination);
+    gainNode.gain.value = volume / 10;
+  }, [volume]);
+
+  useEffect(() => {
+    if (track) track.connect(gainNode).connect(audioContext.destination);
   }, [track]);
+
+
+  // TODO: use this value to both read and write to the song timeline
+  // solution !!!
+  // document.getElementById("audio item").currentTime = 100;
+
+  // also if getting the 'ended' callback is not gonna work, can use this value to see if song over!
 
   return <audio controls {...{ src: `music/${src}`, ref: audioRef, crossOrigin: "anonymous" }} />;
 };
