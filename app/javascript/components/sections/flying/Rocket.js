fb2eadf1e0163aafec184a67b087bab5fc857c7b
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-06-29 22:22:27 +0100

    rocket fire wip

--- app/javascript/components/sections/flying/Rocket.js
+++ app/javascript/components/sections/flying/Rocket.js
@@ -11,6 +11,7 @@ const Rocket = () => {
 
   return (
     <Spacing
+      pointer
       vertical
       {...{
         height: "33vh",
@@ -189,32 +190,39 @@ const MainThurster = () => {
   const { black, white } = useTheme();
 
   return (
-    <Spacing horizontal {...{ height: "100%", width: "100%" }}>
-      <Spacing
-        {...{
-          width: `${6 * SPACING}px`,
-          height: `100%`,
-          background: "#b9c8d4",
-          border: `1px solid ${black}`,
-          boxShadow: `10px -1px 0px -1px ${black} inset`,
-          css: css`
-            border-right: 1px solid ${white};
-            border-top: 1px solid #b9c8d4;
-          `,
-        }}
-      />
-      <Spacing
-        {...{
-          width: `${6 * SPACING}px`,
-          height: `100%`,
-          background: "#e5ecf0",
-          border: `1px solid ${white}`,
-          boxShadow: `-10px -1px 0px -1px ${white} inset`,
-          css: css`
-            border-left: 1px solid ${black};
-          `,
-        }}
-      />
+    <Spacing vertical>
+      <Spacing horizontal {...{ height: "100%", width: "100%" }}>
+        <Spacing
+          {...{
+            width: `${6 * SPACING}px`,
+            height: `100%`,
+            background: "#b9c8d4",
+            border: `1px solid ${black}`,
+            boxShadow: `10px -1px 0px -1px ${black} inset`,
+            css: css`
+              border-right: 1px solid ${white};
+              border-top: 1px solid #b9c8d4;
+            `,
+          }}
+        />
+        <Spacing
+          {...{
+            width: `${6 * SPACING}px`,
+            height: `100%`,
+            background: "#e5ecf0",
+            border: `1px solid ${white}`,
+            boxShadow: `-10px -1px 0px -1px ${white} inset`,
+            css: css`
+              border-left: 1px solid ${black};
+            `,
+          }}
+        />
+      </Spacing>
+      <Fire />
     </Spacing>
   );
+};
+
+const Fire = () => {
+  return <div>fire</div>;
 };
