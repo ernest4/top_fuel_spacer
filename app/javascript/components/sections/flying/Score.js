e229f921d0fefc85fbbabe0d1f5f60cf19b3570b
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-06-29 08:17:30 +0100

    counter optimizations

--- app/javascript/components/sections/flying/Score.js
+++ app/javascript/components/sections/flying/Score.js
@@ -1,49 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
-import Spacing from "../../layout/Spacing";
-import Text from "../../layout/Text";
-import { useSelector, useDispatch, batch } from "react-redux";
+// import Spacing from "../../layout/Spacing"; // These are too heavy weight...
+// import Text from "../../layout/Text"; // These are too heavy weight...
+import { useDispatch, batch, useSelector } from "react-redux";
 import * as scoreActions from "../../store/actions/score";
+import styled from "styled-components";
 
-const Score = () => {
-  const [distance, setDistance] = useState(0);
-
-  // const distance = useSelector(state => state.score.distance);
-  // const speed = useSelector(state => state.score.speed);
-
-  // TODO: move out the incrementer so it gets rerendered little as possible !!!
-
-  const onDistance = newDistance => setDistance(newDistance);
-
-  // TODO: redux is too slow. will need to custom render all the counters. See about sending a local
-  // callback with value to this component
-
-  // TODO: needs more optimization. CPU target, 15% !!!
-  return (
-    <Spacing vertical>
-      <Text white {...{ children: `${distance} meters` }} />
-      {/* <Text white {...{ children: `${speed} per second` }} /> */}
-      <Text white {...{ children: `??? acceleration` }} />
-      <Incrementer {...{ onDistance }} />
-    </Spacing>
-  );
-};
-
-export default Score;
-
-const Incrementer = ({ onDistance: onDistanceCallback }) => {
-  const [distance, setDistance] = useState(0);
+// Highly optimized custom component to deal with constant updates to progress
+const Score = ({ onDistance: onDistanceCallback }) => {
   const disptach = useDispatch();
 
+  const [distance, setDistance] = useState(0);
+
   // const distance = useSelector(state => state.score.distance);
-  // const speed = useSelector(state => state.score.speed);
+  const speed = useSelector(state => state.score.speed);
   // // const acceleration = useSelector(state => state.score.acceleration);
 
   const requestRef = useRef();
   const previousTimeRef = useRef();
   const updateInterval = useRef(0);
   const reduxUpdateInterval = useRef(0);
-
-  const speed = 100; // testing
 
   useEffect(() => {
     const updateScore = time => {
@@ -57,20 +32,22 @@ const Incrementer = ({ onDistance: onDistanceCallback }) => {
         // counter running fast. Internal game state can update at this speed of 5 updates per
         // second, but the counter will have to be done with raw refs (cookie clicker way?)
         if (UPDATE_INTERVAL < updateInterval.current) {
-          updateInterval.current = 0;
+          // console.log(updateInterval.current);
+          // console.log(updateInterval.current / 1000);
+          // console.log(speed * (updateInterval.current / 1000));
 
-          // distanceRef.current = distanceRef.current + speedRef.current * deltaTime;
-          setDistance(distance => Math.round(distance + speed * deltaTime));
-          // console.log(distance);
+          setDistance(distance => distance + speed * (updateInterval.current / 1000));
 
           if (REDUX_UPDATE_INTERVAL < reduxUpdateInterval.current) {
-            reduxUpdateInterval.current = 0;
-
             batch(() => {
               disptach(scoreActions.setDistance(distance));
               // disptach(scoreActions.setSpeed(speed + acceleration * deltaTime));
             });
+
+            reduxUpdateInterval.current = 0;
           }
+
+          updateInterval.current = 0;
         }
       }
 
@@ -81,16 +58,43 @@ const Incrementer = ({ onDistance: onDistanceCallback }) => {
     requestRef.current = window.requestAnimationFrame(updateScore);
 
     return () => window.cancelAnimationFrame(requestRef.current);
-  }, [disptach, distance]);
+  }, [disptach, distance, speed]);
   // }, [disptach, distance, speed, distanceRef, speedRef]);
 
   useEffect(() => {
     if (distance && onDistanceCallback) onDistanceCallback(distance);
   }, [distance, onDistanceCallback]);
 
-  // return <div />;
-  return <div children={distance} />;
+  return (
+    <Container>
+      {/* <div children={distance} /> */}
+      <Distance children={`${formatNumberToSiUnit(distance)}meters`} />
+      <div>testy</div>
+    </Container>
+  );
 };
 
-const REDUX_UPDATE_INTERVAL = 500;
-const UPDATE_INTERVAL = 16; // 60fps
+export default Score;
+
+const REDUX_UPDATE_INTERVAL = 1000;
+const UPDATE_INTERVAL = 50; // 20fps
+
+const Container = styled.div`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  top: 15vh;
+  left: 0px;
+  width: 33vw;
+`;
+
+const Distance = styled.div`
+  /* position: absolute; */
+  /* top: 15vh; */
+`;
+
+const formatNumberToSiUnit = count => {
+  // TODO: use light years once far enough
+  // return `${count} kilo`;
+  return `${count} `;
+};
