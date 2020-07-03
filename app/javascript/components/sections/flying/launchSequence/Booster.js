9cbd43e997d48374c3203d34e3314657088c47d1
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-07-03 21:50:28 +0100

    launchers timing

--- app/javascript/components/sections/flying/launchSequence/Booster.js
+++ app/javascript/components/sections/flying/launchSequence/Booster.js
@@ -7,19 +7,13 @@ import { useSelector } from "react-redux";
 const Booster = ({ right }) => {
   const currentStageId = useSelector(state => state.launchSequence.currentStageId);
   const name = useSelector(state => state.launchSequence.stages[currentStageId]?.name);
-  // TESTING, will come from redux
-  // const [liftOff, setLiftOff] = useState(false);
-  // const [detach, setDetach] = useState(false);
-
-  // const testy = () => {
-  //   if (liftOff) setDetach(true);
-  //   else setLiftOff(true);
-  // };
+  const stagesLength = useSelector(state => state.launchSequence.stages.length);
 
   const liftOff = name === "lift off";
   const detach = name === "boosters detach";
 
-  // TODO: remove this from DOM on last stage
+  if (currentStageId === stagesLength - 1) return null;
+
   return (
     <Spacing
       {...{
