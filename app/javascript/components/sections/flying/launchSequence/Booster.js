9927fdd9ee4ed8cf9d4f0d6bcf20c082183a1f26
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-07-06 09:00:32 +0100

    launch rays progress, need rotation speed tweak

--- app/javascript/components/sections/flying/launchSequence/Booster.js
+++ app/javascript/components/sections/flying/launchSequence/Booster.js
@@ -3,6 +3,7 @@ import Spacing from "../../../layout/Spacing";
 import Beam from "../rocket/Beam";
 import { css, keyframes } from "styled-components";
 import { useSelector } from "react-redux";
+import Triangle from "../../../svg/Triangle";
 
 const Booster = ({ right }) => {
   const currentStageId = useSelector(state => state.launchSequence.currentStageId);
@@ -38,6 +39,9 @@ const Booster = ({ right }) => {
         <>
           {Array.from(Array(5)).map((circle, key) => {
             return <CircleCharger {...{ key, right, index: key }} />;
+          })}
+          {Array.from(Array(1)).map((ray, key) => {
+            return <RayCharger {...{ key, right, index: key }} />;
           })}
         </>
       )}
@@ -90,5 +94,62 @@ const CircleCharger = ({ right, index }) => {
         `,
       }}
     />
+  );
+};
+
+const RayCharger = ({ right, index }) => {
+  const duration = 2;
+
+  return (
+    <Spacing
+      {...{
+        position: "fixed",
+        absoluteBottom: "26vh",
+        absoluteLeft: right ? "10vw" : "4vw",
+        background: "transparent",
+        // background: "green",
+        // border: "32px solid #fec200",
+        // borderRadius: "100%",
+        width: "200px",
+        height: "200px",
+        opacity: "0.5",
+        align: "center",
+        css: css`
+          animation-name: ${keyframes`
+                0% { transform: rotate(0deg) scale(0); opacity: 0;}
+                50% { transform: rotate(360deg) scale(2); opacity: 0.5;}
+                100%  { transform: rotate(7200deg) scale(4); opacity: 1;}
+            `};
+
+          ${"" /* animation-timing-function: cubic-bezier(0.74, 0.07, 1, -0.19); */}
+          animation-timing-function: linear;
+          animation-iteration-count: 1;
+          animation-fill-mode: both;
+          animation-duration: ${duration * 8}s;
+          animation-delay: ${(duration * index) / 2 + 1.5}s;
+        `,
+      }}
+    >
+      <Spacing
+        {...{
+          position: "absolute",
+          background: "#fec200",
+          width: "10px",
+          height: "100%",
+          transform: "rotate(0deg)",
+          borderRadius: "100%",
+        }}
+      />
+      <Spacing
+        {...{
+          position: "absolute",
+          background: "#fec200",
+          width: "10px",
+          height: "100%",
+          transform: "rotate(90deg)",
+          borderRadius: "100%",
+        }}
+      />
+    </Spacing>
   );
 };
