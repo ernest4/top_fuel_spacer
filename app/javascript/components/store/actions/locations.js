2fa522b44213011800123a22a3efdb79e1c1daca
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-07-06 21:47:36 +0100

    action

--- app/javascript/components/store/actions/locations.js
+++ app/javascript/components/store/actions/locations.js
@@ -1,3 +1,4 @@
 import { createAction } from "redux-actions";
 
 export const setCurrentLocationId = createAction("SET_CURRENT_LOCATION_ID");
+export const generateNextLocations = createAction("GENERATE_NEXT_LOCATIONS");
