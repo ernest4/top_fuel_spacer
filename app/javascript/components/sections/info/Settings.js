a2720ddc6564f56fc757b8b3b5b123d0927545b8
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-07-14 21:02:47 +0100

    settngs page auto generation

--- app/javascript/components/sections/info/Settings.js
+++ app/javascript/components/sections/info/Settings.js
@@ -2,13 +2,15 @@ import React from "react";
 import Card from "../../layout/Card";
 import { useSelector } from "react-redux";
 import { capitalize } from "lodash";
+import Text from "../../layout/Text";
+import Spacing from "../../layout/Spacing";
 
 const Settings = () => {
   const settings = useSelector(state => state.settings);
 
   return (
     <Card
-      split // TODO: more intelligent split option ...
+      split
       {...{
         header: { title: "Settings" },
         body: Object.entries(settings).map(([category, categorySettings]) => {
@@ -16,7 +18,7 @@ const Settings = () => {
             <Card
               {...{
                 header: { subtitles: [capitalize(category)] },
-                body: <div>{`${JSON.stringify(categorySettings)}`}</div>,
+                body: <CategorySettings {...{ categorySettings }} />,
               }}
             />
           );
@@ -28,6 +30,50 @@ const Settings = () => {
 };
 
 export default Settings;
+
+const CategorySettings = ({ categorySettings }) => {
+  return (
+    <>
+      {Object.entries(categorySettings).map(([section, sectionSettings]) => {
+        return (
+          <Spacing top={1}>
+            <Text small children={section} />
+            <Spacing top={1} />
+            <SectionSettings {...{ sectionSettings }} />
+          </Spacing>
+        );
+      })}
+    </>
+  );
+};
+
+const SectionSettings = ({ sectionSettings }) => {
+  return (
+    <>
+      {Object.entries(sectionSettings).map(([setting, value]) => {
+        return (
+          <Spacing horizontal>
+            <Text small children={setting} />
+            <Spacing left={1} />
+            <Control {...{ value }} />
+          </Spacing>
+        );
+      })}
+    </>
+  );
+};
+
+const Control = ({ value }) => {
+  // TODO: auto determine control type from value type
+
+  // typeof true => 'boolean'
+  // typeof 5 => 'number'
+  // typeof 'gfdgf' => 'string'
+  // typeof {} => 'object'
+  // typeof (() => {}) => 'function'
+
+  return <div>{`${value}`}</div>;
+};
 
 // const initialState = {
 //   graphics: {
