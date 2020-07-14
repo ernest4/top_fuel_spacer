3e2d1db1edf463ad1ab4bf36a0d710c42c5a76a4
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-07-14 23:02:42 +0100

    wip info page

--- app/javascript/components/sections/info/Info.js
+++ app/javascript/components/sections/info/Info.js
@@ -1,13 +1,57 @@
 import React from "react";
 import Card from "../../layout/Card";
+import Button from "../../misc/Button";
 
 const Info = () => {
   return (
     <Card
+      split
       {...{
-        header: {
-          title: "Info",
-        },
+        header: { title: "Info" },
+        body: [
+          <Card
+            {...{
+              header: { subtitles: ["wip2"] },
+              body: "some text here wip2",
+            }}
+          />,
+          <Card
+            {...{
+              header: { subtitles: ["wip2"] },
+              body: "some text here wip2",
+            }}
+          />,
+          <Card
+            {...{
+              header: { subtitles: ["Planned Future Features"] },
+              body: "some text here wip",
+              footer: (
+                <Button
+                  right
+                  small
+                  tertiary
+                  {...{
+                    link: "https://trello.com/b/lEMbH9W8/top-fuel-spacer",
+                    children: "Trello",
+                    innerProps: { center: true },
+                  }}
+                />
+              ),
+            }}
+          />,
+          <Card
+            {...{
+              header: { subtitles: ["wip2"] },
+              body: "some text here wip2",
+            }}
+          />,
+          <Card
+            {...{
+              header: { subtitles: ["wip2"] },
+              body: "some text here wip2",
+            }}
+          />,
+        ],
       }}
     />
   );
