1aa3b0ea35d6a4517953abe7ead28366521c8dcd
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-07-17 22:27:34 +0100

    achievement hover helper done

--- app/javascript/components/layout/pane/Body.js
+++ app/javascript/components/layout/pane/Body.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Text from "../Text";
 import Spacing from "../Spacing";
+import Line from "../Line";
 
 const Body = ({ text }) => {
   return <Text extraSmall children={textToComponents({ text })} />;
@@ -10,7 +11,7 @@ export default Body;
 
 const textToComponents = ({ text }) => {
   let tokens = text.split(
-    /(<s>[a-zA-Z0-9\s,%@\.]+<\/s>)|(<p>[a-zA-Z0-9\s,%@\.]+<\/p>)|(<d>[a-zA-Z0-9\s,%@\.]+<\/d>)|(<space \/>)/
+    /(<s>[a-zA-Z0-9\s,%@\.\(\)\[\]\|]+<\/s>)|(<p>[a-zA-Z0-9\s,%@\.\(\)\[\]\|]+<\/p>)|(<d>[a-zA-Z0-9\s,%@\.\(\)\[\]\|]+<\/d>)|(<space \/>)/
   );
   tokens = tokens.filter(token => token); // Remove 'undefined'
 
@@ -20,19 +21,20 @@ const textToComponents = ({ text }) => {
 const componentize = string => {
   let match;
 
-  if ((match = string.match(/^<p>([a-zA-Z0-9\s,%@\.]+)<\/p>/)?.[1])) {
+  if ((match = string.match(/^<p>([a-zA-Z0-9\s,%@\.\(\)\[\]\|]+)<\/p>/)?.[1])) {
     return <Text primary extraSmall bold children={match} />;
   }
 
-  if ((match = string.match(/^<s>([a-zA-Z0-9\s,%@\.]+)<\/s>/)?.[1])) {
+  if ((match = string.match(/^<s>([a-zA-Z0-9\s,%@\.\(\)\[\]\|]+)<\/s>/)?.[1])) {
     return <Text secondary extraSmall bold children={match} />;
   }
 
-  if ((match = string.match(/^<d>([a-zA-Z0-9\s,%@\.]+)<\/d>/)?.[1])) {
+  if ((match = string.match(/^<d>([a-zA-Z0-9\s,%@\.\(\)\[\]\|]+)<\/d>/)?.[1])) {
     return <Text danger extraSmall bold children={match} />;
   }
 
   if ((match = string.match(/^(<space \/>)/)?.[1])) return <Spacing top={1} />;
+  if ((match = string.match(/^(<line \/>)/)?.[1])) return <Line />;
 
   // TODO: if ... more
 
--- app/javascript/components/sections/info/indexPage/Progress.js
+++ app/javascript/components/sections/info/indexPage/Progress.js
@@ -2,6 +2,9 @@ import React from "react";
 import { normalizeRequiredCompleted } from "../../../store/reducers/achievements";
 import useTheme from "../../../hooks/useTheme";
 import ProgressBar from "../../../misc/ProgressBar";
+import Container from "../../../layout/Container";
+import Text from "../../../layout/Text";
+import Card from "../../../layout/Card";
 
 const Progress = ({ required, completed }) => {
   const { required: range, completed: value } = normalizeRequiredCompleted({ required, completed });
@@ -10,7 +13,52 @@ const Progress = ({ required, completed }) => {
 
   const barBackground = range === value ? primary : null;
 
-  return <ProgressBar {...{ barBackground, value, range }} />;
+  return (
+    <ProgressBar
+      {...{ barBackground, value, range, hover: <Hover {...{ required, completed }} /> }}
+    />
+  );
 };
 
 export default Progress;
+
+const Hover = ({ required, completed }) => {
+  if (typeof required === "number") return null;
+
+  const body = required
+    .map((requiredItem, key) => {
+      const done = completed.some(completedItem => completedItem === requiredItem);
+
+      if (done) return `<p>${requiredItem} | DONE</p>`;
+
+      return `<s>${requiredItem}</s>`;
+    })
+    .join("<space /><line /><space />");
+
+  return <Card border right noDivider {...{ body }} />;
+};
+
+// ALTERNATIVE APPROACH...less prefered due to break in consistancy between other hovers
+// const Hover = ({ required, completed }) => {
+//   if (typeof required === "number") return null;
+
+//   return (
+//     <Container split border right>
+//       {required.map((requiredItem, key) => {
+//         const done = completed.some(completedItem => completedItem === requiredItem);
+
+//         return (
+//           <Text
+//             extraSmall
+//             {...{
+//               key,
+//               primary: done,
+//               secondary: !done,
+//               children: `${requiredItem}${done ? " | DONE" : ""}`,
+//             }}
+//           />
+//         );
+//       })}
+//     </Container>
+//   );
+// };
